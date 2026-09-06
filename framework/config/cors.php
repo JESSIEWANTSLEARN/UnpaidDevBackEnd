c9dceda8f7fe70788534969f80e7b75f455a2749
JESSIEWANTSLEARN <palaraojessie19@gmail.com>
2026-09-07 02:06:10 +0800

    feat: configure CORS for standalone frontend

---
+++ framework/config/cors.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+
+    'paths' => [
+        'api/*',
+        'login',
+        'login/*',
+        'logout',
+        'register',
+        'signup/*',
+        'forgot-password/*',
+    ],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => [
+        env('FRONTEND_URL', 'http://127.0.0.1:5173'),
+    ],
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => true,
+
+];
